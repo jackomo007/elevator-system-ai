f7533e06c961a030e2e7e651689ba06bf1bd7821
Angel <jeal.code47@gmail.com>
2025-11-29 09:55:22 -0300

    build: initial configuration

---
+++ ElevatorSystem.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace ElevatorSystem.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ ElevatorSystem.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace ElevatorSystem.Tests.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
